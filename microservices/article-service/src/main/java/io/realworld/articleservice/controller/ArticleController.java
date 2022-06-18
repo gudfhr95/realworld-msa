59489ade290428c73118421658887dfb25ef7939
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-06-18 19:39:51 +0900

    :sparkles: add Get Single Article

--- microservices/article-service/src/main/java/io/realworld/articleservice/controller/ArticleController.java
+++ microservices/article-service/src/main/java/io/realworld/articleservice/controller/ArticleController.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,6 +52,13 @@ public class ArticleController {
         body.getBody(), body.getTagList(), username);
 
     return makeResponse(article, username, request.getHeader(AUTHORIZATION));
+  }
+
+  @GetMapping("/articles/{slug}")
+  public ArticleDto getSingleArticle(@PathVariable String slug, HttpServletRequest request) {
+    Article article = articleService.findArticleBySlug(slug).orElseThrow();
+
+    return makeResponse(article, article.getAuthor(), request.getHeader(AUTHORIZATION));
   }
 
   @PutMapping("/articles/{slug}")
